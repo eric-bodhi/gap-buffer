f1994f0daa9520bbab0e10c5c470015748a5510a
Eric Floyd <erixefb@gmail.com>
2024-09-30 22:38:20 -0400

    Update .gitignore and remove tracked directories

--- build/_deps/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/eric/github/gap-buffer/build/_deps/googletest-src/googlemock
-# Build directory: /home/eric/github/gap-buffer/build/_deps/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
